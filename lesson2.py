128c496d38ecfb3209fda409eabf256a214bda19
Martozhyna <mybonanzaluck@gmail.com>
2023-03-06 19:22:10 +0200

    fix hw2

--- lesson2.py
+++ lesson2.py
@@ -55,40 +55,40 @@
 # expanded_form(42) # return '40 + 2'
 # expanded_form(70304) # return '70000 + 300 + 4'
 
-# def expanded_form(num: int) -> str:
-#     num = str(num)
-#     numeric: list[str] = []
-#     st: str = ' '
-#     for i, ch in enumerate(num):
-#         if ch != 0:
-#             numeric.append(ch + '0' * (len(num) - i - 1))
-#             st = ' + '.join(numeric)
-#     print(st)
-#     return st
-#
-#
-# expanded_form(12345)
+def expanded_form(num: int) -> str:
+    num = str(num)
+    numeric: list[str] = []
+    st: str = ' '
+    for i, ch in enumerate(num):
+        if ch != "0":
+            numeric.append(ch + '0' * (len(num) - i - 1))
+            st = ' + '.join(numeric)
+    print(st)
+    return st
+
+
+expanded_form(1010)
 
 # 4) створити декоратор котрий буде підраховувати скільки разів була запущена функція продекорована цим декоратором, та буде виводити це значення після виконання функцій
-def counter(func):
-    count = 0
-
-    def inner(*args, **kwargs):
-        func(*args, **kwargs)
-        nonlocal count
-        count += 1
-        print('count: ', count)
-        return count
-
-    return inner
-
-
-@counter
-def greeting(name):
-    print('hello', name)
-
-
-greeting('max')
-greeting('max')
-greeting('max')
-greeting('veronica')
+# def counter(func):
+#     count = 0
+#
+#     def inner(*args, **kwargs):
+#         func(*args, **kwargs)
+#         nonlocal count
+#         count += 1
+#         print('count: ', count)
+#         return count
+#
+#     return inner
+#
+#
+# @counter
+# def greeting(name):
+#     print('hello', name)
+#
+#
+# greeting('max')
+# greeting('max')
+# greeting('max')
+# greeting('veronica')
